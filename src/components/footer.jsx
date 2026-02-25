dfdc2542113489e7bbb059a999f7947a8704814d
koti23223 <ramakotisayempu2@gmail.com>
2026-02-25 10:19:13 +0530

    Local update

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -9,7 +9,7 @@ export function Footer() {
           <div className="col-md-4">
             <h5>LIFESTYLE</h5>
             <p>Crafting quality essentials for mindful living.</p>
-          </div>
+          </div>n
 
           <div className="col-md-4">
             <h6>Shop</h6>
